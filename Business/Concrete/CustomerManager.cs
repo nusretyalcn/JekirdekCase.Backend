c9c06648345ed9e6231434fd44af5c30ea7f0f2e
Nusret Yalçın <yalcnusret@gmail.com>
2025-03-25 00:32:31 +0300

    Business Service added

---
+++ Business/Concrete/CustomerManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        private readonly ICustomerDal _customerDal;
+
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        public List<Customer> GetList()
+        {
+            var result = _customerDal.GetAll();
+            return result.ToList();
+        }
+
+
+        public void Add(Customer customer)
+        {
+
+            DateOnly.FromDateTime(DateTime.UtcNow);
+            _customerDal.Add(customer);
+        }
+
+        public void Delete(Customer customer)
+        {
+            _customerDal.Delete(customer);
+        }
+
+        public void Update(Customer customer)
+        {
+            _customerDal.Update(customer);
+        }
+    }
+}
