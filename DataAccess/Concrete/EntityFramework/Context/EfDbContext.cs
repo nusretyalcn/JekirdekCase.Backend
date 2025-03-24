dae6f4d8b2f86e7d237c73f3b0934578faa3368e
Nusret Yalçın <yalcnusret@gmail.com>
2025-03-25 00:20:48 +0300

    EfDbContext implemented and Customer entity Added

---
+++ DataAccess/Concrete/EntityFramework/Context/EfDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class EfDbContext:DbContext
+    {
+        private readonly IConfiguration Configuration;
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
